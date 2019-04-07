470776bd5e897961c0e63b76923cd173b34c80a6
Glebs <jjb15133@strath.ac.uk>
2019-04-07 03:47:34 +0100

    Refactoring APIs

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def getLast():
 
 
 @app.route('/api/get/<int:start>/<int:end>', methods=['GET'])
-def get(start, end):
+def get_all(start, end):
     if end - start <= 0:
         return jsonify(results=[{"error": "Invalid timestamps"}])
     elif end - start > 10800:
@@ -60,34 +60,87 @@ def get(start, end):
         return create_response()
 
 
-@app.route('/api/count', methods=['GET'])
-def count():
+@app.route('/api/get/<int:start>/<int:end>/<int:sid>', methods=['GET'])
+def get(start, end, sid):
+    if end - start <= 0:
+        return jsonify(results=[{"error": "Invalid timestamps"}])
+    elif end - start > 10800:
+        return jsonify(results=[{"error": "Time period is too large"}])
+    else:
+        tstart = datetime.datetime.fromtimestamp(start)
+        tend = datetime.datetime.fromtimestamp(end)
+        filedir = '/home/gprohorovs/flask-sensor-data-app/download'
+
+        results = db.session.query(SensorDataModel)
+        rows = [i.serialize for i in results
+            .order_by(SensorDataModel.sampleId.asc())
+            .filter(SensorDataModel.sessionId == sid)
+            .filter(SensorDataModel.time >= tstart)
+            .filter(SensorDataModel.time <= tend)
+            .all()]
+
+        with open(os.path.join(filedir, 'result.json'), 'w') as fp:
+            j = json.dumps(rows, default=converter, indent=4)
+            fp.write(j)
+        return create_response()
+
+
+@app.route('/api/count/<int:sid>', methods=['GET'])
+def count(sid):
+    numRows = db.session.query(SensorDataModel).filter(SensorDataModel.sessionId == sid).count()
+    return 'Number of records for session ' + str(sid) + ': ' + str(numRows)
+
+
+@app.route('/api/countAll', methods=['GET'])
+def count_all():
     numRows = db.session.query(SensorDataModel).count()
-    return 'The total number of rows in the table is ' + str(numRows)
+    return 'The total number of rows: ' + str(numRows)
+
+
+@app.route('/api/delete/<int:sid>')
+def delete(sid):
+    numRows = db.session.query(SensorDataModel) \
+        .filter(SensorDataModel.sessionId == sid) \
+        .delete()
+    db.session.commit()
+    return 'Number of records for session ' + str(sid) + ' deleted: ' + str(numRows)
 
 
 @app.route('/api/deleteAll')
-def deleteAll():
+def delete_all():
     numRows = db.session.query(SensorDataModel).delete()
     db.session.commit()
     return 'The total number of rows deleted ' + str(numRows)
 
 
-@app.route('/api/validate', methods=['GET'])
-def validate():
+@app.route('/api/validate/<int:sid>', methods=['GET'])
+def validate(sid):
     tstart = time.time()
-    errors = []
 
     first = db.session.query(SensorDataModel.sampleId) \
+        .filter(SensorDataModel.sessionId == sid) \
         .order_by(SensorDataModel.sampleId.asc()) \
         .first()
 
     if first is None:
         return 'No records in the table'
     else:
-        counter = first[0]
+        response = [
+            {'time taken': str(time.time() - tstart)},
+            {'errors': validate_dataset(first[0], sid)}]
+        return jsonify(results=response)
+
+
+def converter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+
+def validate_dataset(counter, sid):
+    errors = []
 
     for sample in db.session.query(SensorDataModel.sampleId) \
+            .filter(SensorDataModel.sessionId == sid) \
             .order_by(SensorDataModel.sampleId.asc()) \
             .all():
         sampleId = int(sample[0])
@@ -101,16 +154,7 @@ def validate():
                           + ' rows are missing!')
         counter = counter + 1
 
-    response = [
-        {'time taken': str(time.time() - tstart)},
-        {'errors': errors}
-    ]
-    return jsonify(results=response, indent=4)
-
-
-def converter(o):
-    if isinstance(o, datetime.datetime):
-        return o.__str__()
+    return errors
 
 
 def create_response():
