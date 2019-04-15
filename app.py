c4085d3c1306aa360f6d0f0fab87defed8364e65
Glebs <jjb15133@strath.ac.uk>
2019-04-15 23:06:25 +0100

    Buttons click listeners 27

--- app.py
+++ app.py
@@ -179,8 +179,12 @@ def serialise(results):
 
 def validate_dataset(counter, sid):
     errors = []
-    for sample in get_samples_by_session_id(sid):
+    for sample in db.session.query(SensorDataModel.sampleId) \
+            .filter(SensorDataModel.sessionId == sid) \
+            .order_by(SensorDataModel.sampleId.asc()) \
+            .all():
         sample_id = int(sample[0])
+
         if counter != sample_id:
             difference = (sample_id - counter)
             counter = counter + difference
